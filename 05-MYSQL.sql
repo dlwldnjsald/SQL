b0dd6d1672cb813bd9fe7f27cee5ca6329016fd6
dlwldnjsald <163090571+dlwldnjsald@users.noreply.github.com>
2024-05-09 17:27:47 +0900

    Update 05-MYSQL.sql
    auto_increment 전까지 복습 및 코드 가독성 위한 정리  0509 17:27

--- 05-MYSQL.sql
+++ 05-MYSQL.sql
@@ -8,9 +8,11 @@ SHOW TABLES;
 -- 테이블 구조 확인 
 DESCRIBE actor;
 
--- 간단한 쿼리 실행 
+-- 간단한 쿼리 실행 해보기
 SELECT VERSION(), CURRENT_DATE;
 SELECT VERSION(), CURRENT_DATE FROM dual; 
+-- VERSION() 함수는 현재 MySQL 서버의 버전을 반환
+-- CURRENT_DATE 함수는 현재 날짜를 반환
 -- 위 두 쿼리 중 선택해서 출력 가능 
 -- from dual은 있어도되고 없어도 됨 시스템정보를 나타내는 가상 테이블  
 
@@ -23,11 +25,13 @@ SELECT * FROM actor;
 CREATE DATABASE webdb;
 -- 생성 잘 되었는가 확인
 SHOW DATABASES;
--- 시스템 설정에 좌우되는 경우 많다
+-- 시스템 설정에 좌우되는 경우 많다 적절한 옵션 선택이 중요함
 
 -- 데이터베이스 삭제
 DROP DATABASE webdb;
+
 -- 문자셋, 정렬 방식을 명시적으로 지정하는것이 좋다
+-- webdb라는 데이터베이스 생성하고, 문자셋 utf8mb4, 정렬 방식 utf8mb4_unicode_ci로 명시적 지정
 CREATE DATABASE webdb CHARSET utf8mb4
 	COLLATE utf8mb4_unicode_ci;
 SHOW DATABASES;
@@ -35,19 +39,19 @@ SHOW DATABASES;
 
 -- 사용자 관리>>
 -- 사용자 만들기 
+--  'dev'사용자를 'localhost'에서 만들고, 비밀번호 'dev'로 설정. 
+-- 그러나 아직 이 사용자에게 권한이 부여되지 않아서 생성후 권한부여 필수
 CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
 -- 사용자 비번 변경 
--- ALTER USER 'dev'@'localhost' IDENTIFIED BY 'new_password';
+-- ALTER USER 'dev'@'localhost' IDENTIFIED BY 'new_password'; 하면 됨
 -- 사용자 삭제 
--- DROP USER 'dev'@'localhost';
+-- DROP USER 'dev'@'localhost'; 하면 됨
 -- ---------------------------------
-
 -- 권한>>
 -- 권한의 부여
 -- GRANT 권한목록 ON 객체 TO '계정'@'접속호스트';
 -- 권환 회수
 -- REVOKE 권한 목록 ON 객체 FROM '계정'@'접속호스트';
-
 -- 'dev'@'localhost'에게 /webdb 데이터베이스의 모든 객체에 대한 /모든 권한 허용
 GRANT ALL PRIVILEGES ON webdb.* TO 'dev'@'localhost'; 
 -- 권한 회수시
@@ -73,12 +77,12 @@ SHOW CREATE TABLE author;
 
 -- book 테이블 생성
 CREATE TABLE book (
-	book_id int PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
+	book_id int PRIMARY KEY,  
+    title VARCHAR(100) NOT NULL, 	-- > pk, not null 등 컬럼에 대한 제약사항 표기해준것 
     pubs VARCHAR(100),
-    pub_date DATETIME DEFAULT now(),
+    pub_date DATETIME DEFAULT now(), -- DEFAULT now(): 데이터 삽입시 현재시간으로 자동 설정 //current_timestamp
     author_id int,
-    CONSTRAINT fk_book FOREIGN KEY (author_id)
+    CONSTRAINT fk_book FOREIGN KEY (author_id) -- > constraint,reference 이하문은 테이블 제약사항 표기
     REFERENCES author(author_id)
 ); 
 SHOW TABLES;
@@ -91,24 +95,22 @@ DESC book;
 INSERT INTO author VALUES(1, '박경리', '토지 작가');
 -- 방법2)명시적 방법 : 컬럼 목록 제공, 컬럼 목록의  숫자, 순서, 타입이 
 -- 값 목록의 숫자, 순서, 타입과 일치해야 함
-INSERT INTO author (author_id, author_name) -- author_desc는 널 허용한다하니 빼도됨
+INSERT INTO author (author_id, author_name) -- author_desc는 널 허용한다고 지정해줬기때문에 빼도됨
 VALUES (2, '김영하');
 SELECT * FROM author; -- 목록확인
 
 -- AUTOCOMMIT 꺼주기>>----------------
--- MySQL은 기본적으로 자동 커밋이 활성화
--- autocommit을 비활성화 하기 위해선 autocommit 옵션을 0으로 설정해줘야 한다
-SET autocommit = 0;
--- ---------------------------------
-
+-- MySQL은 기본적으로 자동 커밋 모드가 활성화 -> 각각 쿼리문이 자동커밋되어 db에 즉시반영된단의미
+-- autocommit을 비활성화하면 MySQL은 자동으로 커밋하지 않고 트랜잭션을 수동으로 관리해야 함 
 -- MySQL은 명시적 트랜잭션을 수행한다
-START transaction;
-SELECT * FROM author;
+-- 트랜잭션을 명시적으로 시작하고, 이후 해당 트랜잭션 범위 내에서 모든 작업이 커밋되거나 롤백되도록 해야 함
 
+SET autocommit = 0; -- autocommit을 비활성화 하기 위해선 autocommit 옵션을 0으로 설정
+START transaction; -- 명시적으로 트랜잭션 시작
+SELECT * FROM author;
 -- UPDATE author
 -- SET author_desc = '알쓸신잡 출연'; -- where절이 없을경우 전체 레코드 변경됨 
 -- 위의 경우 where절 안쓰면 안되도록 막아버림
-
 UPDATE author
 SET author_desc = '알쓸신잡 출연'
 WHERE author_id = 2;  -- where절 써줘야 함 
